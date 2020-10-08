145d83b3cfae513ef4ad903e76e4e873d19d2876
David Burkett <davidburkett38@gmail.com>
2020-10-08 08:03:37 -0400

    Improving scheduler test

--- test/tests/common/Test_Scheduler.cpp
+++ test/tests/common/Test_Scheduler.cpp
@@ -10,18 +10,18 @@ TEST_CASE("Scheduler")
     auto increment = [](int& timesRun) { timesRun++; };
 
     int firstTask = 0;
-    pScheduler->RunEvery(std::bind(increment, std::ref(firstTask)), std::chrono::milliseconds(50));
+    pScheduler->RunEvery(std::bind(increment, std::ref(firstTask)), std::chrono::milliseconds(100));
 
     int secondTask = 0;
-    pScheduler->RunEvery(std::bind(increment, std::ref(secondTask)), std::chrono::milliseconds(150));
+    pScheduler->RunEvery(std::bind(increment, std::ref(secondTask)), std::chrono::milliseconds(300));
 
     int thirdTask = 0;
-    pScheduler->RunEvery(std::bind(increment, std::ref(thirdTask)), std::chrono::milliseconds(500));
+    pScheduler->RunEvery(std::bind(increment, std::ref(thirdTask)), std::chrono::milliseconds(1000));
 
     int fourthTask = 0;
     pScheduler->RunOnce(std::bind(increment, std::ref(fourthTask)));
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(225));
+    std::this_thread::sleep_for(std::chrono::milliseconds(450));
     pScheduler.reset();
 
     REQUIRE(firstTask == 4);
