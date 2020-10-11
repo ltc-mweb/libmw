ab939d6cd1fe3f523f846b4ccbe17960d4e88552
Hector Chu <hectorchu@gmail.com>
2020-10-11 15:26:31 +0100

    Use new TxBuilder

--- test/tests/models/tx/Test_Transaction.cpp
+++ test/tests/models/tx/Test_Transaction.cpp
@@ -1,116 +1,53 @@
 #include <catch.hpp>
 
-#include <mw/crypto/Crypto.h>
-#include <mw/crypto/Random.h>
-#include <mw/models/tx/Transaction.h>
 #include <mw/consensus/BlockSumValidator.h>
 
-#include <test_framework/models/TxOutput.h>
+#include <test_framework/TxBuilder.h>
 
 TEST_CASE("Tx Transaction")
 {
     const uint64_t pegInAmount = 123;
     const uint64_t fee = 5;
 
-    BlindingFactor blind_a = Random::CSPRNG<32>().GetBigInt();
-    BlindingFactor blind_b = Random::CSPRNG<32>().GetBigInt();
-    BlindingFactor blind_c = Random::CSPRNG<32>().GetBigInt();
-    BlindingFactor blind_d = Random::CSPRNG<32>().GetBigInt();
+    mw::Transaction::CPtr tx = test::TxBuilder()
+        .AddInput(20).AddInput(30, EOutputFeatures::PEGGED_IN)
+        .AddOutput(45).AddOutput(pegInAmount, EOutputFeatures::PEGGED_IN)
+        .AddPlainKernel(fee).AddPeginKernel(pegInAmount)
+        .Build();
 
-    BlindingFactor offset_a = Random::CSPRNG<32>().GetBigInt();
-    BlindingFactor offset_b = Random::CSPRNG<32>().GetBigInt();
-    BlindingFactor totalOffset = Crypto::AddBlindingFactors({ offset_a, offset_b }, { });
-
-    std::vector<Input> inputs{
-        Input(EOutputFeatures::DEFAULT_OUTPUT, Crypto::CommitBlinded(20, blind_a)),
-        Input(EOutputFeatures::PEGGED_IN, Crypto::CommitBlinded(30, blind_b))
-    };
-    std::vector<Output> outputs{
-        test::TxOutput::Create(EOutputFeatures::DEFAULT_OUTPUT, blind_c, 45).GetOutput(),
-        test::TxOutput::Create(EOutputFeatures::PEGGED_IN, blind_d, pegInAmount).GetOutput()
-    };
-
-    std::vector<Kernel> kernels;
-    {
-        BlindingFactor kernelBF = Crypto::AddBlindingFactors({ blind_c }, { blind_a, blind_b, offset_a });
-        Commitment kernelCommit = Crypto::CommitBlinded(0, kernelBF);
-
-        Serializer serializer;
-        serializer.Append<uint8_t>((uint8_t)KernelType::PLAIN_KERNEL);
-        serializer.Append<uint64_t>(fee);
-
-        Signature signature = Crypto::BuildSignature(
-            kernelBF.ToSecretKey(),
-            Hashed(serializer.vec())
-        );
-
-        kernels.push_back(Kernel::CreatePlain(fee, std::move(kernelCommit), std::move(signature)));
-    }
-    {
-        BlindingFactor kernelBF = Crypto::AddBlindingFactors({ blind_d }, { offset_b });
-        Commitment kernelCommit = Crypto::CommitBlinded(0, kernelBF);
-
-        Serializer serializer;
-        serializer.Append<uint8_t>((uint8_t)KernelType::PEGIN_KERNEL);
-        serializer.Append<uint64_t>(pegInAmount);
-
-        Signature signature = Crypto::BuildSignature(
-            kernelBF.ToSecretKey(),
-            Hashed(serializer.vec())
-        );
-
-        kernels.push_back(Kernel::CreatePegIn(pegInAmount, std::move(kernelCommit), std::move(signature)));
-    }
-
-    TxBody txBody{
-        std::vector<Input>(inputs),
-        std::vector<Output>(outputs),
-        std::vector<Kernel>(kernels)
-    };
-    txBody.Validate();
-
-    mw::Transaction tx{
-        BlindingFactor(totalOffset),
-        TxBody(txBody)
-    };
-    BlockSumValidator::ValidateForTx(tx);
+    BlockSumValidator::ValidateForTx(*tx);
 
     //
     // Serialization
     //
     {
-        std::vector<uint8_t> serialized = tx.Serialized();
+        std::vector<uint8_t> serialized = tx->Serialized();
         REQUIRE(serialized.size() == 1772);
 
         Deserializer deserializer(serialized);
-        REQUIRE(BlindingFactor::Deserialize(deserializer) == totalOffset);
-        REQUIRE(TxBody::Deserialize(deserializer) == txBody);
+        REQUIRE(BlindingFactor::Deserialize(deserializer) == tx->GetOffset());
+        REQUIRE(TxBody::Deserialize(deserializer) == tx->GetBody());
 
         Deserializer deserializer2(serialized);
-        REQUIRE(tx == mw::Transaction::Deserialize(deserializer2));
+        REQUIRE(*tx == mw::Transaction::Deserialize(deserializer2));
     }
 
     //
     // JSON
     //
     {
-        Json json(tx.ToJSON());
+        Json json(tx->ToJSON());
         REQUIRE(json.GetKeys() == std::vector<std::string>({ "body", "offset" }));
-        REQUIRE(BlindingFactor::FromHex(json.GetRequired<std::string>("offset")) == totalOffset);
-        REQUIRE(json.GetRequired<TxBody>("body") == txBody);
+        REQUIRE(BlindingFactor::FromHex(json.GetRequired<std::string>("offset")) == tx->GetOffset());
+        REQUIRE(json.GetRequired<TxBody>("body") == tx->GetBody());
 
-        REQUIRE(tx == mw::Transaction::FromJSON(json));
+        REQUIRE(*tx == mw::Transaction::FromJSON(json));
     }
 
     //
     // Getters
     //
     {
-        REQUIRE(tx.GetOffset() == totalOffset);
-        REQUIRE(tx.GetBody() == txBody);
-        REQUIRE(tx.GetInputs() == inputs);
-        REQUIRE(tx.GetOutputs() == outputs);
-        REQUIRE(tx.GetKernels() == kernels);
-        REQUIRE(tx.GetTotalFee() == fee);
+        REQUIRE(tx->GetTotalFee() == fee);
     }
 }
--- test/tests/models/tx/Test_TxBody.cpp
+++ test/tests/models/tx/Test_TxBody.cpp
@@ -1,68 +1,19 @@
 #include <catch.hpp>
 
-#include <mw/crypto/Crypto.h>
-#include <mw/crypto/Random.h>
-#include <mw/models/tx/TxBody.h>
-
-#include <test_framework/models/TxOutput.h>
+#include <test_framework/TxBuilder.h>
 
 TEST_CASE("Tx Body")
 {
     const uint64_t pegInAmount = 123;
     const uint64_t fee = 5;
 
-    BlindingFactor blind_a = Random::CSPRNG<32>().GetBigInt();
-    BlindingFactor blind_b = Random::CSPRNG<32>().GetBigInt();
-    BlindingFactor blind_c = Random::CSPRNG<32>().GetBigInt();
-    BlindingFactor blind_d = Random::CSPRNG<32>().GetBigInt();
+    mw::Transaction::CPtr tx = test::TxBuilder()
+        .AddInput(20).AddInput(30, EOutputFeatures::PEGGED_IN)
+        .AddOutput(45).AddOutput(pegInAmount, EOutputFeatures::PEGGED_IN)
+        .AddPlainKernel(fee).AddPeginKernel(pegInAmount)
+        .Build();
 
-    std::vector<Input> inputs{
-        Input(EOutputFeatures::DEFAULT_OUTPUT, Crypto::CommitBlinded(20, blind_a)),
-        Input(EOutputFeatures::PEGGED_IN, Crypto::CommitBlinded(30, blind_b))
-    };
-    std::vector<Output> outputs{
-        test::TxOutput::Create(EOutputFeatures::DEFAULT_OUTPUT, blind_c, 45).GetOutput(),
-        test::TxOutput::Create(EOutputFeatures::PEGGED_IN, blind_d, pegInAmount).GetOutput()
-    };
-
-    std::vector<Kernel> kernels;
-    // For simplification no offsets are used.
-    {
-        BlindingFactor kernelBF = Crypto::AddBlindingFactors({ blind_c }, { blind_a, blind_b });
-        Commitment kernelCommit = Crypto::CommitBlinded(0, kernelBF);
-
-        Serializer serializer;
-        serializer.Append<uint8_t>((uint8_t)KernelType::PLAIN_KERNEL);
-        serializer.Append<uint64_t>(fee);
-
-        Signature signature = Crypto::BuildSignature(
-            kernelBF.ToSecretKey(),
-            Hashed(serializer.vec())
-        );
-
-        kernels.push_back(Kernel::CreatePlain(fee, std::move(kernelCommit), std::move(signature)));
-    }
-    {
-        BlindingFactor kernelBF = Crypto::AddBlindingFactors({ blind_d }, { });
-        Commitment kernelCommit = Crypto::CommitBlinded(0, kernelBF);
-
-        Serializer serializer;
-        serializer.Append<uint8_t>((uint8_t)KernelType::PEGIN_KERNEL);
-        serializer.Append<uint64_t>(pegInAmount);
-
-        Signature signature = Crypto::BuildSignature(
-            kernelBF.ToSecretKey(),
-            Hashed(serializer.vec())
-        );
-
-        kernels.push_back(Kernel::CreatePegIn(pegInAmount, std::move(kernelCommit), std::move(signature)));
-    }
-
-    TxBody txBody{
-        std::vector<Input>(inputs),
-        std::vector<Output>(outputs),
-        std::vector<Kernel>(kernels)
-    };
+    const TxBody& txBody = tx->GetBody();
     txBody.Validate();
 
     //
@@ -73,9 +24,9 @@ TEST_CASE("Tx Body")
         REQUIRE(serialized.size() == 1740);
 
         Deserializer deserializer(serialized);
-        REQUIRE(deserializer.Read<uint64_t>() == inputs.size());
-        REQUIRE(deserializer.Read<uint64_t>() == outputs.size());
-        REQUIRE(deserializer.Read<uint64_t>() == kernels.size());
+        REQUIRE(deserializer.Read<uint64_t>() == txBody.GetInputs().size());
+        REQUIRE(deserializer.Read<uint64_t>() == txBody.GetOutputs().size());
+        REQUIRE(deserializer.Read<uint64_t>() == txBody.GetKernels().size());
 
         Deserializer deserializer2(serialized);
         REQUIRE(txBody == TxBody::Deserialize(deserializer2));
@@ -87,9 +38,9 @@ TEST_CASE("Tx Body")
     {
         Json json(txBody.ToJSON());
         REQUIRE(json.GetKeys() == std::vector<std::string>({ "inputs", "kernels", "outputs" }));
-        REQUIRE(json.GetRequiredVec<Input>("inputs") == inputs);
-        REQUIRE(json.GetRequiredVec<Output>("outputs") == outputs);
-        REQUIRE(json.GetRequiredVec<Kernel>("kernels") == kernels);
+        REQUIRE(json.GetRequiredVec<Input>("inputs") == txBody.GetInputs());
+        REQUIRE(json.GetRequiredVec<Output>("outputs") == txBody.GetOutputs());
+        REQUIRE(json.GetRequiredVec<Kernel>("kernels") == txBody.GetKernels());
 
         REQUIRE(txBody == TxBody::FromJSON(json));
     }
@@ -98,9 +49,6 @@ TEST_CASE("Tx Body")
     // Getters
     //
     {
-        REQUIRE(txBody.GetInputs() == inputs);
-        REQUIRE(txBody.GetOutputs() == outputs);
-        REQUIRE(txBody.GetKernels() == kernels);
         REQUIRE(txBody.GetTotalFee() == fee);
     }
 }
